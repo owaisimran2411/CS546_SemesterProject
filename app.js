0d1c69cf975b3b848966198feddf12574c1347ab
Jonathan Memoli <jonomemoli3@gmail.com>
2024-04-19 16:55:11 -0400

    Added update functions for users collecion
    Added check to make sure that a username or email doesn't already exist before creating a user

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const handlebarsInstance = exphbs.create({
 
 app.use('/public', staticDir);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', handlebarsInstance.engine);
 app.set('view engine', 'handlebars');
@@ -49,22 +49,22 @@ const { PORT_NUMBER } = process.env
 async function mainR() {
   try {
     const renamedIPhonePro = await methods.userSignUp(
-      
-        "username1",
-        "password",
-        "security ques 1",
-        "security ans 1",
-        "security question 2",
-        "security answer2",
-        "email",
-        2334132132,
-        "male",
-        "My bio",
-        "profile pic"
+
+      "username2",
+      "password2",
+      "security ques 3",
+      "security ans 3",
+      "security question 4",
+      "security answer 4",
+      "email2",
+      2334132132,
+      "male",
+      "My bio 2",
+      "profile pic"
     );
     console.log(renamedIPhonePro);
   } catch (error) {
-    console.error("Error adding user:", error.message); // Log the error message
+    console.error("Error adding user:", error); // Log the error message
   }
 }
 mainR();
@@ -72,24 +72,88 @@ mainR();
 async function mainP() {
   try {
     const renamedIPhonePro = await prodMethod.default.createProduct(
-      
-        "prod_name1",
-        "description",
-        "condition",
-        "serial no.",
-        33,
-        [ "Nintendo Wii" ],
-        "thumbnail",
-       [ "otherImg"],
-        true,
-        "660a1f58411aa9ec268e4412",
+
+      "prod_name1",
+      "description",
+      "condition",
+      "serial no.",
+      33,
+      ["Nintendo Wii"],
+      "thumbnail",
+      ["otherImg"],
+      true,
+      "660a1f58411aa9ec268e4412",
     );
     console.log(renamedIPhonePro);
   } catch (error) {
     console.error("Error adding product:", error); // Log the error message
   }
 }
-//mainP();
+// mainP();
+async function testUpdate() {
+  try {
+    const updatedUser = await methods.updateUsername("6620241c33657dc262689cba", 'newUsername');
+    console.log(updatedUser);
+  }
+  catch (e) {
+    console.log(e);
+  }
+  try {
+    const updatedUser = await methods.updatePassword("6620241c33657dc262689cba", 'newPassword');
+    console.log(updatedUser);
+  }
+  catch (e) {
+    console.log(e);
+  }
+  try {
+    const updatedUser = await methods.updateEmailAddress("6620241c33657dc262689cba", 'newEmail');
+    console.log(updatedUser);
+  }
+  catch (e) {
+    console.log(e);
+  }
+  try {
+    const updatedUser = await methods.updatePhoneNumber("6620241c33657dc262689cba", '9082111234');
+    console.log(updatedUser);
+  }
+  catch (e) {
+    console.log(e);
+  }
+  try {
+    const updatedUser = await methods.updateGender("6620241c33657dc262689cba", 'female');
+    console.log(updatedUser);
+  }
+  catch (e) {
+    console.log(e);
+  }
+  try {
+    const updatedUser = await methods.updateBio("6620241c33657dc262689cba", 'newBio');
+    console.log(updatedUser);
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+// testUpdate();
+async function testFind() {
+  try {
+    console.log('Find with username:');
+    const user = await methods.getUserByUsername('username2');
+    console.log(user);
+  }
+  catch (e) {
+    console.log(e);
+  }
+  try {
+    console.log('Find with email:');
+    const user = await methods.getUserByEmail('email2');
+    console.log(user);
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+// testFind();
 
 
 
--- data/user.js
+++ data/user.js
@@ -2,116 +2,231 @@ import { users } from "../configuration/mongoCollections.js";
 import * as helperMethods from "./../helper.js";
 import { ObjectId } from "mongodb";
 // Data function goes here
-
+const { argumentProvidedValidation, primitiveTypeValidation } = helperMethods;
 // below function is a template function, rename it!
 const userSignUp = async (
-  username,
-  password,
-  securityQuestionOne,
-  securityAnswerOne,
-  securityQuestionTwo,
-  securityAnswerTwo,
-  emailAddress,
-  phoneNumber,
-  gender,
-  bio,
-  profilePicture
+	username,
+	password,
+	securityQuestionOne,
+	securityAnswerOne,
+	securityQuestionTwo,
+	securityAnswerTwo,
+	emailAddress,
+	phoneNumber,
+	gender,
+	bio,
+	profilePicture
 ) => {
-  const { argumentProvidedValidation, primitiveTypeValidation } = helperMethods;
-  // validating all the arguments
-  argumentProvidedValidation(username, "Username");
-  argumentProvidedValidation(password, "Password");
-  argumentProvidedValidation(securityQuestionOne, "Security Question 1");
-  argumentProvidedValidation(securityAnswerOne, "Security Answer 1");
-  argumentProvidedValidation(securityQuestionTwo, "Security Question 2");
-  argumentProvidedValidation(securityAnswerTwo, "Security Answer 2");
-  argumentProvidedValidation(emailAddress, "Email Address");
-  argumentProvidedValidation(phoneNumber, "Phone Number");
-  argumentProvidedValidation(gender, "Gender");
-  argumentProvidedValidation(bio, "Bio");
-  argumentProvidedValidation(profilePicture, "Profile Picture");
+	const { argumentProvidedValidation, primitiveTypeValidation } = helperMethods;
+	// validating all the arguments
+	argumentProvidedValidation(username, "Username");
+	argumentProvidedValidation(password, "Password");
+	argumentProvidedValidation(securityQuestionOne, "Security Question 1");
+	argumentProvidedValidation(securityAnswerOne, "Security Answer 1");
+	argumentProvidedValidation(securityQuestionTwo, "Security Question 2");
+	argumentProvidedValidation(securityAnswerTwo, "Security Answer 2");
+	argumentProvidedValidation(emailAddress, "Email Address");
+	argumentProvidedValidation(phoneNumber, "Phone Number");
+	argumentProvidedValidation(gender, "Gender");
+	argumentProvidedValidation(bio, "Bio");
+	argumentProvidedValidation(profilePicture, "Profile Picture");
 
-  //validating data types and overriding them after removing spaces
-  username = primitiveTypeValidation(username, "Username", "String");
-  password = primitiveTypeValidation(password, "Password", "String");
-  securityQuestionOne = primitiveTypeValidation(
-    securityQuestionOne,
-    "Security Question 1",
-    "String"
-  );
-  securityAnswerOne = primitiveTypeValidation(
-    securityAnswerOne,
-    "Security Answer 1",
-    "String"
-  );
-  securityQuestionTwo = primitiveTypeValidation(
-    securityQuestionTwo,
-    "Security Question 2",
-    "String"
-  );
-  securityAnswerTwo = primitiveTypeValidation(
-    securityAnswerTwo,
-    "Security Answer 2",
-    "string"
-  );
-  emailAddress = primitiveTypeValidation(
-    emailAddress,
-    "Email Address",
-    "String"
-  );
-  phoneNumber = primitiveTypeValidation(phoneNumber, "Phone Number", "Number");
-  gender = primitiveTypeValidation(gender, "Gender", "String");
-  bio = primitiveTypeValidation(bio, "Bio", "String");
-  profilePicture = primitiveTypeValidation(
-    profilePicture,
-    "Profile Picture",
-    "String"
-  );
+	//validating data types and overriding them after removing spaces
+	username = primitiveTypeValidation(username, "Username", "String");
+	password = primitiveTypeValidation(password, "Password", "String");
+	securityQuestionOne = primitiveTypeValidation(
+		securityQuestionOne,
+		"Security Question 1",
+		"String"
+	);
+	securityAnswerOne = primitiveTypeValidation(
+		securityAnswerOne,
+		"Security Answer 1",
+		"String"
+	);
+	securityQuestionTwo = primitiveTypeValidation(
+		securityQuestionTwo,
+		"Security Question 2",
+		"String"
+	);
+	securityAnswerTwo = primitiveTypeValidation(
+		securityAnswerTwo,
+		"Security Answer 2",
+		"string"
+	);
+	emailAddress = primitiveTypeValidation(
+		emailAddress,
+		"Email Address",
+		"String"
+	);
+	phoneNumber = primitiveTypeValidation(phoneNumber, "Phone Number", "Number");
+	gender = primitiveTypeValidation(gender, "Gender", "String");
+	bio = primitiveTypeValidation(bio, "Bio", "String");
+	profilePicture = primitiveTypeValidation(
+		profilePicture,
+		"Profile Picture",
+		"String"
+	);
 
-  const newUser = {
-    username: username,
-    password: password,
-    securityQuestionOne: securityQuestionOne,
-    securityAnswerOne: securityAnswerOne,
-    securityQuestionTwo: securityQuestionTwo,
-    securityAnswerTwo: securityAnswerTwo,
-    emailAddress: emailAddress,
-    phoneNumber: phoneNumber,
-    gender: gender,
-    bio: bio,
-    profilePicture: profilePicture,
-  };
+	const newUser = {
+		username: username,
+		password: password,
+		securityQuestionOne: securityQuestionOne,
+		securityAnswerOne: securityAnswerOne,
+		securityQuestionTwo: securityQuestionTwo,
+		securityAnswerTwo: securityAnswerTwo,
+		emailAddress: emailAddress,
+		phoneNumber: phoneNumber,
+		gender: gender,
+		bio: bio,
+		profilePicture: profilePicture,
+	};
 
-  const userCollection = await users();
-  const newInsertInformation = await userCollection.insertOne(newUser);
-  if (!newInsertInformation.insertedId) throw "Insert failed!";
-  return await getUserById(newInsertInformation.insertedId.toString());
+	const userCollection = await users();
+	// check if username already exists
+	const existingUser = await userCollection.findOne({ username: username });
+	if (existingUser) {
+		throw "Username already exists";
+	}
+
+	// check if password already exists 
+	const existingPassword = await userCollection.findOne({ password: password });
+	if (existingPassword) {
+		throw "Password already exists";
+	}
+	const newInsertInformation = await userCollection.insertOne(newUser);
+	if (!newInsertInformation.insertedId) throw "Insert failed!";
+	return await getUserById(newInsertInformation.insertedId.toString());
 };
 
 const getUserById = async (id) => {
-  id = helperMethods.checkId(id)
-  const userCollection = await users();
-  const user = await userCollection.findOne({ _id: new ObjectId(id) });
-  if (!user) throw "Error: User not found";
-  return user;
+	id = helperMethods.checkId(id)
+	const userCollection = await users();
+	const user = await userCollection.findOne({ _id: new ObjectId(id) });
+	if (!user) throw "Error: User not found";
+	return user;
 };
 
-const getUserByEmail = async (email) => {
-  const userCollection = await users();
-  const user = await userCollection.findOne({ emailAddress: email });
-  if (!user) throw "Error: User not found";
-  return user;
+const getUserByEmail = async (emailAddress) => {
+	argumentProvidedValidation(emailAddress, "Email Address");
+	emailAddress = primitiveTypeValidation(
+		emailAddress,
+		"Email Address",
+		"String"
+	);
+
+	const userCollection = await users();
+	const user = await userCollection.findOne({ emailAddress: emailAddress });
+	if (!user) throw "Error: User not found";
+	return user;
 }
 
 const getUserByUsername = async (username) => {
-  const userCollection = await users();
-  const user = await userCollection.findOne({ username: username });
-  if (!user) throw "Error: User not found";
-  return user;
-}
+	argumentProvidedValidation(username, "Username");
+	username = primitiveTypeValidation(username, "Username", "String");
 
+	const userCollection = await users();
+	const user = await userCollection.findOne({ username: username });
+	if (!user) throw "Error: User not found";
+	return user;
+}
+const updateUsername = async (id, username) => {
+	id = helperMethods.checkId(id);
+	argumentProvidedValidation(username, "Username");
+	username = primitiveTypeValidation(username, "Username", "String");
+
+	const userCollection = await users();
+	const updatedUser = await userCollection.findOneAndUpdate(
+		{ _id: new ObjectId(id) },
+		{ $set: { username: username } },
+		{ returnDocument: 'after' }
+	);
+	if (!updatedUser) throw 'Failed to update username';
+	return updatedUser;
+}
+const updatePassword = async (id, password) => {
+	id = helperMethods.checkId(id);
+	argumentProvidedValidation(password, "Password");
+	password = primitiveTypeValidation(password, "Password", "String");
+
+	const userCollection = await users();
+	const updatedUser = await userCollection.findOneAndUpdate(
+		{ _id: new ObjectId(id) },
+		{ $set: { password: password } },
+		{ returnDocument: 'after' }
+	);
+	if (!updatedUser) throw 'Failed to update password';
+	return updatedUser;
+}
+const updateEmailAddress = async (id, emailAddress) => {
+	id = helperMethods.checkId(id);
+	argumentProvidedValidation(emailAddress, "Email Address");
+	emailAddress = primitiveTypeValidation(
+		emailAddress,
+		"Email Address",
+		"String"
+	);
+	const userCollection = await users();
+	const updatedUser = await userCollection.findOneAndUpdate(
+		{ _id: new ObjectId(id) },
+		{ $set: { emailAddress: emailAddress } },
+		{ returnDocument: 'after' }
+	);
+	if (!updatedUser) throw 'Failed to update email';
+	return updatedUser;
+}
+const updatePhoneNumber = async (id, phoneNumber) => {
+	id = helperMethods.checkId(id);
+	argumentProvidedValidation(phoneNumber, "Phone Number");
+	phoneNumber = primitiveTypeValidation(phoneNumber, "Phone Number", "Number");
+
+	const userCollection = await users();
+	const updatedUser = await userCollection.findOneAndUpdate(
+		{ _id: new ObjectId(id) },
+		{ $set: { phoneNumber: phoneNumber } },
+		{ returnDocument: 'after' }
+	);
+	if (!updatedUser) throw 'Failed to update phone number';
+	return updatedUser;
+}
+const updateGender = async (id, gender) => {
+	id = helperMethods.checkId(id);
+	argumentProvidedValidation(gender, "Gender");
+	gender = primitiveTypeValidation(gender, "Gender", "String");
+
+	const userCollection = await users();
+	const updatedUser = await userCollection.findOneAndUpdate(
+		{ _id: new ObjectId(id) },
+		{ $set: { gender: gender } },
+		{ returnDocument: 'after' }
+	);
+	if (!updatedUser) throw 'Failed to update gender';
+	return updatedUser;
+}
+const updateBio = async (id, bio) => {
+	id = helperMethods.checkId(id);
+	argumentProvidedValidation(bio, "Bio");
+	bio = primitiveTypeValidation(bio, "Bio", "String");
+
+	const userCollection = await users();
+	const updatedUser = await userCollection.findOneAndUpdate(
+		{ _id: new ObjectId(id) },
+		{ $set: { bio: bio } },
+		{ returnDocument: 'after' }
+	);
+	if (!updatedUser) throw 'Failed to update bio';
+	return updatedUser;
+}
 const methods = {
-  userSignUp,
-  getUserById,
+	userSignUp,
+	getUserById,
+	getUserByEmail,
+	getUserByUsername,
+	updateEmailAddress,
+	updatePassword,
+	updateUsername,
+	updatePhoneNumber,
+	updateGender,
+	updateBio
 };
 export default methods;
