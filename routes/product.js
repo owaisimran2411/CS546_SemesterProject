66c54aef55b0c3efb6eb901a7d7efcd8e1c70d51
Owais <owaisimran678@gmail.com>
2024-04-24 22:17:57 -0400

    Code Cleanup

--- routes/product.js
+++ routes/product.js
@@ -6,15 +6,9 @@ import * as helperMethods from './../helper.js'
 
 
 helperMethods.configureDotEnv();
-// const s3Client = 
-// const productCoverImageClient = helperMethods.createMulterObject(s3Client, process.env.S3_BUCKET, 'coverImage')
 
 
 const router = Router()
-
-// router.post('/', , (req, res) => {
-
-// })
 
 router.route('/')
 .post(helperMethods.createMulterObject(helperMethods.createS3Client(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY, process.env.S3_REGION), process.env.S3_BUCKET, 'coverImage').array('file', 5), async (req, res) => {
@@ -26,32 +20,5 @@ router.route('/')
         message: 'Upload Successful'
     })
 })
-// router.route('/')
-// .post(async(req, res) => {
-//     console.log('POST Req received')
-//     helperMethods.configureDotEnv()
-
-//     await 
-    
-//     await helperMethods.handleSingleFileUpload(
-//         req,
-//         'abc',
-//         'profile-picture',
-//         process.env.AWS_ACCESS_KEY_ID,
-//         process.env.AWS_SECRET_ACCESS_KEY,
-//         process.env.S3_REGION,
-//         process.env.S3_BUCKET
-//     ).then(data => {
-//         res.status(200).json({
-//             message: 'success',
-//             data
-//         })
-//     }).catch(error => {
-//         res.status(400).json({
-//             message: 'An error occurred',
-//             error
-//         })
-//     })
-// })
 
 export default router;
