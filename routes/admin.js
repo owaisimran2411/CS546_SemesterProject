6336c9fd700c7f92bc1124811bdb3e406fd20805
Owais <owaisimran678@gmail.com>
2024-05-01 18:56:22 -0400

    Add form to list a new product

--- routes/admin.js
+++ routes/admin.js
@@ -1,43 +1,42 @@
-import {
-    Router
-} from 'express'
-import * as helperMethods from './../helper.js'
+import { Router } from "express";
+import * as helperMethods from "./../helper.js";
 
-import {
-    productData
-} from './../data/index.js'
-
+import { productData } from "./../data/index.js";
 
 helperMethods.configureDotEnv();
 
-const router = Router()
+const router = Router();
 
-router.route('/')
-.get(async (req, res) => {
-    
-})
+router.route("/").get(async (req, res) => {});
 
+router.route("/view-all-products").get(async (req, res) => {
+	const productsListed = await productData.getProducts(
+		true,
+		1,
+		1,
+		{},
+		{},
+		{
+			_id: 1,
+			productName: 1,
+			productAskingPrice: 1,
+			listingActive: 1,
+			productAskingPrice: 1,
+		}
+	);
+	// console.log(productsListed)
+	return res.render("admin/viewProducts", {
+		products: productsListed,
+	});
+});
 
-router.route('/view-all-products')
-.get(async (req, res) => {
-    const productsListed = await productData.getProducts(true, 1, 1, {}, {}, {
-        _id: 1, productName: 1, productAskingPrice: 1, listingActive: 1, productAskingPrice: 1
-    })
-    // console.log(productsListed)
-    return res.render(
-        'admin/viewProducts', {
-            products: productsListed
-        }
-    )
-})
-
-router.route('/product/:productID/:action')
-.get(async (req, res) => {
-    const productUpdate = await productData.updateProductInformation(
-        req.params.productID, {
-            listingActive: (req.params.action === 'enable' ? true : false)
-        }
-    )
-    return res.redirect('/admin/view-all-products')
-})
+router.route("/product/:productID/:action").get(async (req, res) => {
+	const productUpdate = await productData.updateProductInformation(
+		req.params.productID,
+		{
+			listingActive: req.params.action === "enable" ? true : false,
+		}
+	);
+	return res.redirect("/admin/view-all-products");
+});
 export default router;
