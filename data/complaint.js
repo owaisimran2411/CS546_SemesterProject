058b1fd50124bfdc00903dca2b7faeb651271744
Jasmine Cairns <jasmine.cairns26@gmail.com>
2024-05-01 20:48:53 -0400

    Added complaint data functions

--- data/complaint.js
+++ data/complaint.js
@@ -1,13 +1,72 @@
-import * as helperMethods from './../helper.js'
+import * as helperMethods from './../helper.js';
+import { complaints } from '../configuration/mongoCollections.js';
+import { ObjectId } from 'mongodb';
+import { userData } from './index.js';
 
-// Data function goes here
+const createComplaint = async (
+    userId, 
+    sellerId, 
+    complaintText
+) => {
+    const { argumentProvidedValidation, primitiveTypeValidation } = helperMethods;
+    argumentProvidedValidation(userId, "userId");
+    argumentProvidedValidation(sellerId, "sellerId");
+    userId = primitiveTypeValidation(
+        userId,
+        "userId",
+        "String"
+      );
+    sellerId = primitiveTypeValidation(
+        sellerId,
+        "sellerId",
+        "String"
+      );
 
-// below function is a template function, rename it!
-const dataFunction1 = (args) => {
-    console.log("Function def")
-}
+      let complaint = await complaintsCollection.find({
+        userId: ObjectId(userId),
+        sellerId: ObjectId(sellerId)
+      }).toArray();
+      if (complaint.length !== 0) {
+        throw "error: Complaint with given user and seller already exists";
+      };
+
+      const newComplaint = {
+        userId: ObjectId(userId),
+        sellerId: ObjectId(sellerId),
+        complaintText: complaintText,
+        status: "Pending"
+      };
+
+    const user = await userData.getUserById(userId);
+    const seller = await userData.getUserById(sellerId);
+
+    if (!user || !seller) {
+      throw "error: User or seller not found.";
+    }
+
+    const complaintsCollection = await complaints();
+    const insertInfo = await complaintsCollection.insertOne(newComplaint);
+
+    if (insertInfo.insertedCount === 0) {
+      throw "Error: Could not add complaint.";
+    }
+
+    return insertInfo.insertedId;
+
+};
+
+const getComplaintsByUserAndSellerId = async (userId, sellerId) => {
+    const complaintsCollection = await complaints();
+    const userComplaints = await complaintsCollection.find({
+      userId: ObjectId(userId),
+      sellerId: ObjectId(sellerId)
+    }).toArray();
+    return userComplaints;
+};
+
 const methods = {
-    dataFunction1,
-    // append all other functions implemented to export them as default
-}
-export default methods
+  createComplaint,
+  getComplaintsByUserAndSellerId
+};
+
+export default methods;
