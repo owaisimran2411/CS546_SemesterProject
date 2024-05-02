370748b8cdf8d8614f4e09300b707be907bb824b
Owais <owaisimran678@gmail.com>
2024-05-02 16:30:43 -0400

    Add function to update Status of a complaint

--- data/complaint.js
+++ data/complaint.js
@@ -1,72 +1,93 @@
-import * as helperMethods from './../helper.js';
-import { complaints } from '../configuration/mongoCollections.js';
-import { ObjectId } from 'mongodb';
-import { userData } from './index.js';
+import * as helperMethods from "./../helper.js";
+import { complaints } from "../configuration/mongoCollections.js";
+import { ObjectId } from "mongodb";
+import { userData } from "./index.js";
+import { Collection } from "mongoose";
 
-const createComplaint = async (
-    userId, 
-    sellerId, 
-    complaintText
-) => {
-    const { argumentProvidedValidation, primitiveTypeValidation } = helperMethods;
-    argumentProvidedValidation(userId, "userId");
-    argumentProvidedValidation(sellerId, "sellerId");
-    userId = primitiveTypeValidation(
-        userId,
-        "userId",
-        "String"
-      );
-    sellerId = primitiveTypeValidation(
-        sellerId,
-        "sellerId",
-        "String"
-      );
+const createComplaint = async (userId, sellerId, complaintText) => {
+	const { argumentProvidedValidation, primitiveTypeValidation } = helperMethods;
+	argumentProvidedValidation(userId, "userId");
+	argumentProvidedValidation(sellerId, "sellerId");
+	userId = primitiveTypeValidation(userId, "userId", "String");
+	sellerId = primitiveTypeValidation(sellerId, "sellerId", "String");
 
-      let complaint = await complaintsCollection.find({
-        userId: ObjectId(userId),
-        sellerId: ObjectId(sellerId)
-      }).toArray();
-      if (complaint.length !== 0) {
-        throw "error: Complaint with given user and seller already exists";
-      };
+	let complaint = await complaintsCollection
+		.find({
+			userId: userId,
+			sellerId: sellerId,
+		})
+		.toArray();
+	if (complaint.length !== 0) {
+		throw "error: Complaint with given user and seller already exists";
+	}
 
-      const newComplaint = {
-        userId: ObjectId(userId),
-        sellerId: ObjectId(sellerId),
-        complaintText: complaintText,
-        status: "Pending"
-      };
+	const _id = helperMethods.generateObjectID();
+	const newComplaint = {
+		_id: _id,
+		userId: userId,
+		sellerId: sellerId,
+		complaintText: complaintText,
+		status: "Pending",
+	};
 
-    const user = await userData.getUserById(userId);
-    const seller = await userData.getUserById(sellerId);
+	const user = await userData.getUserById(userId);
+	const seller = await userData.getUserById(sellerId);
 
-    if (!user || !seller) {
-      throw "error: User or seller not found.";
-    }
+	if (!user || !seller) {
+		throw "error: User or seller not found.";
+	}
 
-    const complaintsCollection = await complaints();
-    const insertInfo = await complaintsCollection.insertOne(newComplaint);
+	const complaintsCollection = await complaints();
+	const insertInfo = await complaintsCollection.insertOne(newComplaint);
 
-    if (insertInfo.insertedCount === 0) {
-      throw "Error: Could not add complaint.";
-    }
+	if (insertInfo.insertedCount === 0) {
+		throw "Error: Could not add complaint.";
+	}
 
-    return insertInfo.insertedId;
-
+	return _id;
 };
 
 const getComplaintsByUserAndSellerId = async (userId, sellerId) => {
-    const complaintsCollection = await complaints();
-    const userComplaints = await complaintsCollection.find({
-      userId: ObjectId(userId),
-      sellerId: ObjectId(sellerId)
-    }).toArray();
-    return userComplaints;
+	const complaintsCollection = await complaints();
+	const userComplaints = await complaintsCollection
+		.find({
+			userId: userId,
+			sellerId: sellerId,
+		})
+		.toArray();
+	return userComplaints;
+};
+
+const updateComplaintStatus = async (complaintId, updateStatus) => {
+	complaintId = helperMethods.checkId(complaintId);
+	helperMethods.argumentProvidedValidation(updateStatus, "updateStatus");
+	updateStatus = helperMethods.primitiveTypeValidation(
+		updateStatus,
+		"updateStatus",
+		"String"
+	);
+	const complaintsCollection = await complaints();
+	const userComplaintsUpdated = await complaintsCollection.updateOne(
+		{
+			_id: complaintId,
+		},
+		{
+			$set: {
+				status: updateStatus,
+			},
+		}
+	);
+
+	if (userComplaintsUpdated.modifiedCount != 1)
+		throw "Unable to update complaint";
+
+	return "success";
 };
 
 const methods = {
-  createComplaint,
-  getComplaintsByUserAndSellerId
+	createComplaint,
+	getComplaintsByUserAndSellerId,
+	updateComplaintStatus,
 };
 
 export default methods;
