756d44ab411e28f5d00119f64698d740c3fabbf7
Jonathan Memoli <jonomemoli3@gmail.com>
2024-05-02 20:04:38 -0400

    Add complaint form to seller profile page. Also add lookup for js scripts to main.handlebars

---
+++ public/js/form-validation/createComplaint.js
@@ -0,0 +1,33 @@
+$(document).ready(function (){
+    let complaintForm = $('#complaint-form');
+    
+    let currentPath = window.location.href;
+    let url = new URL(currentPath);
+    let segments = url.pathname.split('/');
+    let sellerId = segments[segments.length-1];
+    complaintForm.action = '/complaint/createComplaint/' + sellerId;
+    
+    complaintForm.submit(function(event){
+        event.preventDefault();
+
+        let errorDiv = $('#create-complaint-errors-client');
+        errorDiv.empty();
+
+        let complaintMessage = $('#complaint-message').val();
+
+        if(!complaintMessage){
+            errorDiv.append('<p>You must provide a complaint message</p>');
+            return;
+        }
+        complaintMessage = complaintMessage.trim();
+        if(complaintMessage.length === 0){
+            errorDiv.append('<p>You must provide a complaint message</p>');
+            return;
+        }
+
+
+        complaintForm.append(sellerIdInput);
+
+        complaintForm.get(0).submit();
+    });
+});
