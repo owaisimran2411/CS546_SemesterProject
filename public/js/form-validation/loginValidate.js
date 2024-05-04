d5a12e92b450152e61a80504dc738549d9f780e2
Jonathan Memoli <jonomemoli3@gmail.com>
2024-05-04 02:18:01 -0400

    Add client side checks for login, register, and product complaint forms

---
+++ public/js/form-validation/loginValidate.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+    let loginForm = $('#login-form');
+
+    loginForm.submit(function (event) {
+        event.preventDefault();
+        let errorDiv = $('#login-errors');
+        errorDiv.empty();
+
+        let username = $('#username').val();
+        if (!username) {
+            errorDiv.append('<p>You must provide a username!</p>');
+            return;
+        }
+        username = username.trim();
+        if (username.length === 0) {
+            errorDiv.append('<p>You must provide a username!</p>');
+            return;
+        }
+
+        let password = $('#password').val();
+        if (!password) {
+            errorDiv.append('<p>You must provide a password!</p>');
+            return;
+        }
+        password = password.trim();
+        if (password.length === 0) {
+            errorDiv.append('<p>You must provide a password!</p>');
+            return;
+        }
+        if (/\s/.test(password)) {
+            errorDiv.append('<p>Password should not contain spaces.</p>');
+            return;
+        }
+        const specialCharacters = "!@#$%^&*()-_+=<>?/\\";
+        if (password.length < 8) {
+            errorDiv.append('<p>Password should be at least 8 characters long.</p>');
+            return;
+        }
+        for (let char of password) {
+            if (char >= "A" && char <= "Z") {
+                isUppercase = true;
+            }
+            if (char >= "a" && char <= "z") {
+                isLowercase = true;
+            }
+            if (!isNaN(char)) {
+                isNumber = true;
+            }
+            if (specialCharacters.includes(char)) {
+                isSpecialCharacter = true;
+            }
+        }
+    
+        if (!isUppercase) {
+            errorDiv.append('<p>Password should contain at least one uppercase character.</p>');
+            return;
+        }
+    
+        if (!isLowercase) {
+            errorDiv.append('<p>Password should contain at least one lowercase character.</p>');
+            return;
+        }
+    
+        if (!isNumber) {
+            errorDiv.append('<p>Password should contain at least one number.</p>');
+            return;
+        }
+    
+        if (!isSpecialCharacter) {
+            errorDiv.append('<p>Password should contain at least one special character.</p>');
+            return;
+        }
+    });
+});
---
+++ public/js/form-validation/registerValidate.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    let registerForm = $('#register-form');
+
+    registerForm.submit(function (event) {
+        event.preventDefault();
+        let errorDiv = $('#register-errors');
+        errorDiv.empty();
+
+        // check each text input for blank or missing inputs
+        $('input[type="text"]').each(function () {
+            let input = $(this).val();
+            let id = $(this).attr('id');
+
+            if (!input) {
+                if ((errorDiv).is(':empty')) {
+                    errorDiv.append(`<p>Missing input: ${id}.</p>`);
+                }
+                return;
+            }
+            input = input.trim();
+            if (input.length === 0) {
+                if ((errorDiv).is(':empty')) {
+                    errorDiv.append(`<p>Missing input: ${id}.</p>`);
+                }
+                return;
+            }
+        });
+
+        const phoneNumber = $('#phoneNumber').val().trim();
+        if (!phoneNumber.match(/^\d{10}$/)) {
+            if ((errorDiv.is(':empty'))) {
+                errorDiv.append(`<p>Invalid phone number.</p>`);
+            }
+            return;
+        }
+    });
+});
