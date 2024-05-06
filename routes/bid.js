f4caca9ee65d4f120d4b8897999786b1c6492c78
Jonathan Memoli <jonomemoli3@gmail.com>
2024-05-06 17:06:20 -0400

    Add email and username fields to product bid page

--- routes/bid.js
+++ routes/bid.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as helperMethods from "./../helper.js";
-import { bidData, productData } from "../data/index.js";
+import { bidData, productData, userData } from "../data/index.js";
 const { argumentProvidedValidation, primitiveTypeValidation } = helperMethods;
 
 const router = Router();
@@ -38,15 +38,31 @@ router.route("/createBid").post(async (req, res) => {
 });
 
 router.route("/:productID").get(async (req, res) => {
+	let fullBidInfo = []
 	try {
 		const bids = await bidData.getBidByProductId(req.params.productID);
+		let bidArray = bids.Bids;
+		for(let i = 0; i < bidArray.length; i++){
+			let currentBidObject = bidArray[i];
+			const userId = currentBidObject.userID.toString();
+			const userInfo = await userData.getUserById(userId);
+
+			const bidAmount = currentBidObject.bidAmount;
+
+			const bidObjectToAdd = {
+				username: userInfo.username,
+				email: userInfo.emailAddress,
+				bidAmount: bidAmount
+			}
+			fullBidInfo.push(bidObjectToAdd);
+		}
 		const productInformation = await productData.getProductById(
 			req.params.productID
 		);
 		// console.log(bids, productInformation);
 		return res.render("user/viewProductBids", {
 			docTitle: "View Current Bids",
-			bids: bids.Bids,
+			bids: fullBidInfo,
 			product: productInformation,
 		});
 	} catch (e) {
